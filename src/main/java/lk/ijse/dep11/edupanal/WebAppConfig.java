62e616c59681a6be2fcfaaddad4144dccaa90f60
PrasadPrabash <prasad.lakman@gmail.com>
2023-12-04 17:07:51 +0530

    create lectureReqTO object with validators

--- src/main/java/lk/ijse/dep11/edupanal/WebAppConfig.java
+++ src/main/java/lk/ijse/dep11/edupanal/WebAppConfig.java
@@ -1,11 +1,18 @@
 package lk.ijse.dep11.edupanal;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan
 public class WebAppConfig {
+
+    @Bean
+    public StandardServletMultipartResolver multipartResolver(){  // we have to specify the StandardServletMultipartResolver to convert multipart to java object
+        return new StandardServletMultipartResolver();
+    }
 }
--- src/main/java/lk/ijse/dep11/edupanal/api/LecturerHttpController.java
+++ src/main/java/lk/ijse/dep11/edupanal/api/LecturerHttpController.java
@@ -1,21 +1,26 @@
 package lk.ijse.dep11.edupanal.api;
 
+import lk.ijse.dep11.edupanal.to.request.LecturerReqTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/v1/lecturers")
 @CrossOrigin
 public class LecturerHttpController {
 
-    @PostMapping
-    public void createNewLecturer(){
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(consumes = "multipart/form-data", produces = "application/json")
+    public void createNewLecturer(@ModelAttribute @Valid LecturerReqTO lecturer){
+        System.out.println(lecturer);
         System.out.println("CreateNewLecturer");
     }
 
     @PatchMapping("/{lecturer-id}")
     public void updateLecturerDetails(){
         System.out.println("updateLecturerDetails()");
-
     }
 
     @DeleteMapping("/{lecturer-id}")
